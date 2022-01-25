9856d3fe17df5652d9c7f77173c403076b0951cd
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-01-25 18:46:25 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,38 +1,26 @@
 #include <iostream>
 #include <assert.h>
 #include <vector>
+#include <utility>
+using namespace std;
 
-std::vector<std::string> majorColorToBePrinted;
-std::vector<std::string> minorColorToBePrinted;
+std::vector<std::pair<string ,string> > oColorMaps;
 
-void print()
-{
-	std::cout<<"MajorColor"<<"\n";
-    std::vector<std::string>::iterator itr = majorColorToBePrinted.begin();
-    while(itr!= majorColorToBePrinted.end())
-    {
-        std::cout<<"\n"<<*itr;
-        ++itr;
-    }
-	
-	std::cout<<"\nMinorColor";
-	itr = minorColorToBePrinted.begin();
-    while(itr!= minorColorToBePrinted.end())
-    {
-        std::cout<<"\n"<<*itr;
-        ++itr;
-    }
-}
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
     int i = 0, j = 0;
+    
     for(i = 0; i < 5; i++) 
     {
-        majorColorToBePrinted.push_back(majorColor[i]);
         for(j = 0; j < 5; j++) 
         {
-            minorColorToBePrinted.push_back(minorColor[i]);
+            pair<string, string> Individualpair;
+            Individualpair.first = majorColor[i];
+            Individualpair.second = minorColor[i];
+            oColorMaps.push_back(Individualpair);
+            
             std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
@@ -44,6 +32,12 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    
+    // random check. The second pair in the ColorMap should be "White,Orange"
+    std::pair <string, string> oExpectedColorPairAtSecondIndex ("White","Orange");
+    assert(oExpectedColorPairAtSecondIndex == oColorMaps[1]);
+    
+    
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
