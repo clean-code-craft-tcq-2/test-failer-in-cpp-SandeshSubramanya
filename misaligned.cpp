e7fb775d0514621f1891c236a3b1b31647c2cf06
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-01-25 18:04:59 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
 #include <assert.h>
+#include <vector>
 
+std::vector<std::string> majorColorToBePrinted;
+std::vector<std::string> minorColorToBePrinted;
+
+void print()
+{
+	std::cout<<"MajorColor"<<"\n";
+    std::vector<std::string>::iterator itr = majorColorToBePrinted.begin();
+    while(itr!= majorColorToBePrinted.end())
+    {
+        std::cout<<"\n"<<*itr;
+        ++itr;
+    }
+	
+	std::cout<<"\nMinorColor";
+	itr = minorColorToBePrinted.begin();
+    while(itr!= minorColorToBePrinted.end())
+    {
+        std::cout<<"\n"<<*itr;
+        ++itr;
+    }
+}
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
+    for(i = 0; i < 5; i++) 
+    {
+        majorColorToBePrinted.push_back(majorColor[i]);
+        for(j = 0; j < 5; j++) 
+        {
+            minorColorToBePrinted.push_back(minorColor[i]);
             std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
+    
+    print();
     return i * j;
 }
 
