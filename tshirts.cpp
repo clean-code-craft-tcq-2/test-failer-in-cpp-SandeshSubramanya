1c2e2f6a9c1565e45261947fb5ff3df30ca59634
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-01-25 16:38:19 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -17,6 +17,11 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    std::cout << "All is well (maybe!)\n";
+    
+    // expecting for the size equaling 38 and 42 as M. 
+    assert(size(42) == 'M');
+    assert(size(38) == 'M')
+    
+        std::cout << "All is well (maybe!)\n";
     return 0;
 }
