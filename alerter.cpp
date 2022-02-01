84924ba252b179cff060529dba020df1404f3463
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-02-01 19:43:21 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -8,26 +8,27 @@ int networkAlertStub(float celcius) {
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    if(celcius > 200)
+    if(celcius <= 200.0f)
     {
-        // celcius higher than 200 is okay
-        return 500;     // Not OK
+		// celcius less or equaling 200 is OK
+        return 200;     // OK
     }
     else
-    {   // celcius less or equaling 200 is okay
-        return 200;
+    {
+        // celcius higher than 200 is NOT okay
+        return 500;
     }
 }
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
+    if (returnCode > 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
